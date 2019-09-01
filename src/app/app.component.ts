080e49a847fd661dde398160bb4f1d24b710d32e
O.Maykivets <o.maykivets@viseven.com>
2019-09-01 16:25:41 +0300

    excersize Angular ChangeDetectionStrategy.OnPush - remove unsed import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Stock } from './model/stock';
 
 @Component({
